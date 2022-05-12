e23fec2378606331fa854151a2bb4aa8d92ba817
DevDengChao <2325690622@qq.com>
2022-05-12 11:19:31 +0800

    feat: implement a basic refresher-parser

---
+++ src/refresher-parser.ts
@@ -0,0 +1,65 @@
+import {CdnCacheRefresher} from "./cdn-cache-refresher";
+import {lodash, Logger} from "@serverless-devs/core";
+import {AliyunCdnCacheRefresher} from "./aliyun-cdn-cache-refresher";
+
+let logger = new Logger("refresh-cdn-cache");
+
+
+export class RefresherParser {
+
+    private readonly inputs: object;
+    private readonly args: object;
+
+    constructor(inputs: object, args: object) {
+        this.inputs = inputs;
+        this.args = args;
+    }
+
+    private readonly acceptableCdnValues = "Acceptable values (case insensitive):\r\n" +
+        "\t+ alibaba\r\n" +
+        "\t+ aliyun\r\n";
+
+    public parse(): CdnCacheRefresher {
+        // NOTE: maybe we should use something like multi-key map to do this
+
+        // prefer args.cdn then service.component as user may using CDN at another cloud
+        let cdn = lodash.get(this.args, "cdn", "" /* default to empty string*/).toLowerCase();
+        switch (cdn) {
+            case "":
+                logger.debug(`Cannot determine which cdn cache refresher to use by args.cdn`);
+                break;
+            case "alibaba":
+            case "aliyun":
+                logger.info(`Decided to use aliyun cdn cache refresher as user specified args.cdn with value '${cdn}'`);
+                return new AliyunCdnCacheRefresher();
+            default:
+                throw new Error(JSON.stringify({
+                    message: `Invalid argument cdn value '${cdn}'.`,
+                    tips: this.acceptableCdnValues
+                }));
+        }
+
+        let component = lodash.get(this.inputs, "component");
+        switch (component) {
+            case 'fc':
+            case 'devsapp/fc':
+                logger.info(`Decided to use aliyun cdn cache refresher as user specified service.component with value '${component}'`);
+                return new AliyunCdnCacheRefresher();
+            default:
+                let message = `Cannot determine which cdn cache refresher to use.`;
+                // TODO 2022/5/12: highlight in example
+                throw new Error(JSON.stringify({
+                    message,
+                    tips: "Specify a cdn argument like below or report your issue here: https://github.com/DevDengChao/refresh-cdn-cache\r\n" +
+                        "\r\n" +
+                        "actions:\r\n" +
+                        "   pre-deploy:\r\n" +
+                        "       - plugin: DevDengChao/refresh-cdn-cache\r\n" +
+                        "         args:\r\n" +
+                        "           - cdn: aliyun\r\n" +
+                        "\r\n" +
+                        this.acceptableCdnValues,
+                }));
+        }
+    }
+}
---
+++ test/refresher-parser.spec.ts
@@ -0,0 +1,54 @@
+import {RefresherParser} from "../src/refresher-parser";
+import {AliyunCdnCacheRefresher} from "../src/aliyun-cdn-cache-refresher";
+
+
+test('parse aliyun cdn cache refresher from args.cdn', async function () {
+    let cdns = ["alibaba", "aliyun", "Aliyun" /* also acceptable */];
+    let index = Math.floor(Math.random() * cdns.length);
+    let cdn = cdns[index];
+    // Crypto.randomInt(cdns.length); is not available before nodejs 12.19.0
+    console.log(`trying to parse args.cdn with value ${cdn}`);
+    let parser = new RefresherParser({}, {cdn});
+    let refresher = parser.parse();
+    expect(refresher).toBeInstanceOf(AliyunCdnCacheRefresher);
+});
+
+test('unacceptable args.cdn',  function (done) {
+    let parser = new RefresherParser({}, {
+        cdn: "unacceptable-value"
+    });
+
+    try {
+        parser.parse();
+    } catch (e) {
+        expect(e.message).toContain('message');
+        expect(e.message).toContain('tips');
+        expect(e.message).toContain('Invalid argument cdn value');
+        done();
+    }
+});
+
+test('parse aliyun cdn cache refresher from service.component', async function () {
+    let components = ["fc", "devsapp/fc"];
+    let index = Math.floor(Math.random() * components.length);
+    let component = components[index];
+
+    console.log(`trying to parse service.component with value ${component}`);
+    let parser = new RefresherParser({component}, {});
+    let refresher = parser.parse();
+    expect(refresher).toBeInstanceOf(AliyunCdnCacheRefresher);
+});
+
+
+test('unacceptable service.component',   function (done) {
+    let parser = new RefresherParser({component:"unacceptable-value"}, {});
+    try {
+        parser.parse();
+    } catch (e){
+        expect(e.message).toContain('message');
+        expect(e.message).toContain('tips');
+        expect(e.message).toContain('Cannot determine which cdn cache refresher to use');
+        done();
+    }
+});
+
