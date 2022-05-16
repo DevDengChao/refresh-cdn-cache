c34350c927261fd54e97c6f39e44366553f1622a
DevDengChao <2325690622@qq.com>
2022-05-16 09:34:51 +0800

    docs: add plugin model spec href

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ let logger = new Logger("refresh-cdn-cache");
  * 插件入口
  * @param inputs
  * @param args
+ * @see <a href="https://github.com/Serverless-Devs/Serverless-Devs/blob/master/spec/zh/0.0.2/serverless_package_model/package_model.md#%E6%8F%92%E4%BB%B6%E6%A8%A1%E5%9E%8B%E8%A7%84%E8%8C%83">插件模型规范</a>
  */
 module.exports = async function index(inputs, args) {
     logger.debug(`inputs params: ${JSON.stringify(inputs)}`);
